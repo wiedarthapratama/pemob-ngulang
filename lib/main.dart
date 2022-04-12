f6bdecd1fa7a9c83acfd7acc8cef89e16f35eca1
wied artha pratama <wiedarthapratama@gmail.com>
2022-04-12 19:15:11 +0700

    p3

--- lib/main.dart
+++ lib/main.dart
@@ -19,30 +19,148 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pemob P1',key: key,),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 10,key: key,),
-            Text('Selamat Datang di Universitas Esa Unggul',key: key,),
-            Text('Program Studi Teknik Informatika - Fakultas Ilmu Komputer',key: key,),
-            SizedBox(height: 10,key: key,),
-            Image.network("https://siakad.esaunggul.ac.id/siakad/siakad/index.php?page=download&type=fotomhs&id=eHFJRHV6YzZmaUsydG5ybiUyRmtZb3cwQ3BITkNsdjl4anFFNyUyQnJlN3ZQdlYyTW1GcEh4dnBrdnBLeHdCM29jb0hzRlJ2SWRSSXg1eWdKJTJCTGhLNjJjMWclM0QlM0Q="),
-            SizedBox(height: 10,key: key,),
-            Text('Nama Saya: Wied Artha Pratama',key: key,),
-            Text('NIM Saya: 20180801316',key: key,),
-            Text('Hobby Saya: Bermain game',key: key,),
-            Text('Pekerjaan Saya: Programmer',key: key,),
-            Text('Status Saya: Belum Menikah',key: key,),
-          ],
-        ),
+      // appBar: AppBar(
+      //   title: Text('Pemob P1',key: key,),
+      // ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+          ),
+          Text(
+            'Curriculum Vitae',
+            key: key,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10,
+            key: key,
+          ),
+          Image.network(
+              "https://siakad.esaunggul.ac.id/siakad/siakad/index.php?page=download&type=fotomhs&id=eHFJRHV6YzZmaUsydG5ybiUyRmtZb3cwQ3BITkNsdjl4anFFNyUyQnJlN3ZQdlYyTW1GcEh4dnBrdnBLeHdCM29jb0hzRlJ2SWRSSXg1eWdKJTJCTGhLNjJjMWclM0QlM0Q="),
+          SizedBox(
+            height: 10,
+            key: key,
+          ),
+          Text(
+            'Data Diri',
+            key: key,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 5,
+            key: key,
+          ),
+          Text(
+            'Nama: Wied Artha Pratama',
+            key: key,
+          ),
+          Text(
+            'Jenis Kelamin: Laki-laki',
+            key: key,
+          ),
+          Text(
+            'Tempat Lahir: Banyumas',
+            key: key,
+          ),
+          Text(
+            'Tanggal Lahir: 07 Februari 1999',
+            key: key,
+          ),
+          SizedBox(
+            height: 10,
+            key: key,
+          ),
+          Text(
+            'Data Riwayat Pekerjaan',
+            key: key,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 5,
+            key: key,
+          ),
+          Text(
+            'Perusahaan: PT. My Fashion Grosir',
+            key: key,
+          ),
+          Text(
+            'Jabatan: Programmer',
+            key: key,
+          ),
+          Text(
+            'Periode: November 2021 - Sekarang',
+            key: key,
+          ),
+          SizedBox(
+            height: 5,
+            key: key,
+          ),
+          Text(
+            'Perusahaan: PT. Nawastra Teknologi Nimpuna',
+            key: key,
+          ),
+          Text(
+            'Jabatan: Programmer',
+            key: key,
+          ),
+          Text(
+            'Periode: Agustus 2019 - November 2021',
+            key: key,
+          ),
+          SizedBox(
+            height: 5,
+            key: key,
+          ),
+          Text(
+            'Perusahaan: PT. Wallezz Finansial Teknologi',
+            key: key,
+          ),
+          Text(
+            'Jabatan: Programmer',
+            key: key,
+          ),
+          Text(
+            'Periode: Februari 2018 - Agustus 2019',
+            key: key,
+          ),
+          SizedBox(
+            height: 10,
+            key: key,
+          ),
+          Text(
+            'Data Pendidikan',
+            key: key,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 5,
+            key: key,
+          ),
+          Text(
+            'Universitas - Esa Unggul - (2018-sekarang)',
+            key: key,
+          ),
+          Text(
+            'SMK - SMKS Yuppentek 2 - (2014-2017)',
+            key: key,
+          ),
+          Text(
+            'SMP - SMPN 1 Cikupa - (2011-2014)',
+            key: key,
+          ),
+          Text(
+            'SD - SDN Dukuh 3 - (2005-2011)',
+            key: key,
+          ),
+        ],
       ),
     );
   }
